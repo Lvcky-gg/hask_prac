022b79b66c17f456cd628bd80b5327063cdcf6e5
lvcky-gg <matadorcrab@gmail.com>
2025-04-13 11:15:46 -0400

    chore: func dec

--- Funcs.hs
+++ Funcs.hs
@@ -1,3 +1,9 @@
 inRange :: (Ord a) => a -> a -> a -> Bool
 inRange min max x =
   x >= min && x <= max
+
+inRangeTwo :: (Ord p) => p -> p -> p -> Bool
+inRangeTwo min max x =
+  let inLowerBound = min <= x
+      inUpperBound = max >= x
+   in inLowerBound && inUpperBound
